a0945bedd46c3da53b28f1c4ade8c359b02aca62
mbalandari <m.balandari.toroghi@gmail.com>
2022-01-26 18:53:17 +0300

    correct scroll behavior

--- src/components/BooksContainer/index.js
+++ src/components/BooksContainer/index.js
@@ -1,16 +1,44 @@
+import { debounce } from 'lodash-es';
 import { BookList, Container, H2 } from "./styles";
 import Book from '../Book';
+import { useEffect, useRef, useState } from 'react';
 
 
-const BooksContainer = ({ books, pickBook, isPanelOpen }) => (
-    <Container $isPanelOpen={isPanelOpen}>
-        <H2>All Books</H2>
-        <BookList>
-            {books.map((book) => (
-                <Book key={book.id} book={book} pickBook={pickBook} />
-            ))}
-        </BookList>
-    </Container>
-);
+const BooksContainer = ({ books, pickBook, isPanelOpen }) => {
+    const [scroll, setScroll] = useState(0)
+    const prevPanelState = useRef(false)
 
+    useEffect(() => {
+        const handleScroll = debounce(() => {
+            setScroll(window.scrollY)
+        }, 100)
+
+        if (!isPanelOpen) {
+            window.addEventListener("scroll", handleScroll)
+        }
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, [isPanelOpen])
+
+    useEffect(() => {
+        if (prevPanelState.current && !isPanelOpen) {
+            window.scroll(0, scroll)
+        }
+
+        prevPanelState.current = isPanelOpen
+    }, [isPanelOpen, prevPanelState, scroll])
+
+    return (
+        <Container $isPanelOpen={isPanelOpen} $top={scroll}>
+            <H2>All Books</H2>
+            <BookList>
+                {books.map((book) => (
+                    <Book key={book.id} book={book} pickBook={pickBook} />
+                ))}
+            </BookList>
+        </Container>
+    )
+}
 export default BooksContainer;
