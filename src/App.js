8d45c762ab0c5054812045046dcf2b11b8d1dc5b
mbalandari <m.balandari.toroghi@gmail.com>
2022-01-26 12:59:53 +0300

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 
-function App() {
+
+const App = () => {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://book-club-json.herokuapp.com/books")
+        console.log(`returns fetch req: `, response)
+        const books = await response.json()
+        console.log(`json response: `, books)
+        setBooks(books)
+      } catch (errors) {
+        console.log(errors)
+      }
+    }
+
+
+    fetchData()
+  }, [])
+
+  console.log(`the books: `, books)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hey!
     </div>
   );
 }
 
 export default App;
+
